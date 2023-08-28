f7849fd84d036ad621a452f89cfd09ceb6e34eee
boom-ai <69669572+boom-ai@users.noreply.github.com>
2023-08-28 16:25:05 +0530

    done

--- app/src/main/java/com/sanket/livedata/WorkingFragment.kt
+++ app/src/main/java/com/sanket/livedata/WorkingFragment.kt
@@ -69,6 +69,9 @@ class WorkingFragment : Fragment() {
             }
 
         }
+        binding.btnReset.setOnClickListener {
+            binding.tvNumber.setText("0")
+        }
     }
     companion object {
         /**
